0887db6584ff5f27d140f463d999b7c77b5c1414
yf-yang <directoryyf@gmail.com>
2018-11-23 10:31:23 +0800

    multithread download and convert to npz

--- data_preparation.py
+++ data_preparation.py
@@ -3,6 +3,25 @@ import urllib.request
 import json
 import numpy as np
 import pandas as pd
+from multiprocessing.dummy import Pool
+
+def download_file(name, spec):
+    url = spec['url']
+
+    # All of the datasets are ascii files, so save with extension .txt
+    save_file = 'data/raw/{}.txt'.format(name)
+
+    # file exists
+    if os.path.isfile(save_file):
+        print('Ignore', save_file, ': file exists')
+
+    try:
+        urllib.request.urlretrieve(url, save_file)
+    except Exception as e:
+        print('Got', type(e).__name__, '-', e,
+            '\nIgnore', save_file, ': fail to download')
+    else:
+        print('Succesfully download', save_file)
 
 def download_datasets(data_spec):
     if not os.path.exists('data'):
@@ -11,45 +30,36 @@ def download_datasets(data_spec):
     if not os.path.exists('data/raw'):
         os.makedirs('data/raw')
 
-    # download file and convert to npy
-    for name, spec in data_spec.items():
-        # All of the datasets are text files
-        save_file = 'data/raw/{}.txt'.format(name)
-
-        # file exists
-        if os.path.isfile(save_file):
-            print('Ignore', save_file, ': file exists')
-            continue
-
-        url = spec['url']
-        try:
-            urllib.request.urlretrieve(url, save_file)
-        except Exception as e:
-            print('Failed to download', save_file)
-            raise e
-        else:
-            print('Succesfully download', save_file)
-
+    # download files
+    with Pool(16) as pool:
+        pool.starmap(download_file, data_spec.items())
+        
 def convert_to_npz(data_spec):
-    print('Start Dataset conversion')
+    print()
+    print('-----------------------------------')
+    print('Dataset Conversion')
     for name, spec in data_spec.items():
         raw_file = 'data/raw/{}.txt'.format(name)
         save_file = 'data/{}.npz'.format(name)
 
         # read dataframe
         layout = spec['layout']
-        if layout == 'txt':
-            df = pd.read_csv(
-                    raw_file, 
-                    header=None, delim_whitespace=True, engine='python', 
-                    **spec['kwargs'])
-        elif layout == 'csv':
-            df = pd.read_csv(
-                    raw_file, 
-                    header=None, engine='python', 
-                    **spec['kwargs'])
-        else:
-            raise Exception
+        try:
+            if layout == 'csv':
+                df = pd.read_csv(
+                        raw_file, 
+                        header=None, engine='python', 
+                        **spec['kwargs'])
+            elif layout == 'txt':
+                df = pd.read_csv(
+                        raw_file, 
+                        header=None, delim_whitespace=True, engine='python', 
+                        **spec['kwargs'])
+            elif layout == "special":
+                df = pd.read_csv(raw_file, engine='python', **spec['kwargs'])
+        except FileNotFoundError:
+            print('Ignore', raw_file, ': file not found on disk')
+            continue
 
         # drop null rows
         df = df[(df.values != spec['null_value']).all(axis=1)]
@@ -58,30 +68,36 @@ def convert_to_npz(data_spec):
         for col, method in spec['preprocess']:
             if method == 'comma_string_to_float':
                 df[col] = df[col].replace({',':'.'}, regex=True).astype(float)
-            elif method == 'factorize':
-                df[col] = pd.factorize(df[col])[0]
 
         # split target y
-        y = df[spec["target"]]
+        y = pd.factorize(df[spec["target"]])[0]
 
         # drop unused columns
         X = df.drop(columns=[spec["target"]]+spec['drop_cols'])
 
         # create dummy variables
+        if spec['dummy_cols']:
+            assert spec["target"] not in spec['dummy_cols']
+            dummies = pd.get_dummies(X[spec['dummy_cols']].astype(str), drop_first=True)
+            non_dummies = X.drop(columns=spec['dummy_cols']) 
+            X = pd.concat([non_dummies, dummies], axis=1)
 
-
-        # validate
+        # validate dataset settings
         n, p = X.shape
-        K = len(y.unique())
+        K = len(np.unique(y))
         assert n == spec['n'], ("Dataset size mismatch in {}, "
-            "expected n = {}, but got {}".format(name, spec['n'], n))
+            "expect n = {}, but got {}".format(name, spec['n'], n))
         assert p == spec['p'], ("Feature dimension mismatch in {}, "
-            "expected p = {}, but got {}".format(name, spec['p'], p))
+            "expect p = {}, but got {}".format(name, spec['p'], p))
         assert K == spec['K'], ("Number of class labels mismatch in {}, "
-            "expected K = {}, but got {}".format(name, spec['K'], K))
+            "expect K = {}, but got {}".format(name, spec['K'], K))
+
+        # Convert to float
+        X = X.values.astype(np.float32)
+        y = y.astype(np.int)
 
         np.savez(save_file, X=X, y=y)
-        print("Succesfully saved {}".format(save_file))
+        print("Succesfully save {}".format(save_file))
 
 def preprocess():
     data_spec = json.load(open('filelist.json'))
